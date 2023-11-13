5b031707954341fdfeff30eaae01989944128b23
DanilaTrueman <148095014+DanilaTrueman@users.noreply.github.com>
2023-11-13 13:52:03 +0300

    commit 2.0

--- src/main/java/StudentList/Main.java
+++ src/main/java/StudentList/Main.java
@@ -14,6 +14,7 @@ public class Main {public static void main(String[] args) {
             System.out.println("Сколько студентов участвовало?");
             numStudents = Integer.parseInt(scanner.nextLine());
             validInput = true;
+
         } catch (NumberFormatException e) {
             System.out.println("Введите пожалуйста число.");
         }
